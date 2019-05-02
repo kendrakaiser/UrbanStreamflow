3aa50646c500c15f9db55e451f780bef51bda55f
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-05-02 17:06:34 -0600

    Find files

--- Process_PROSPER.R
+++ Process_PROSPER.R
@@ -1,3 +1,11 @@
 # PROSPER DATA ANALYSIS
 
 #import BRB.tiff data
+
+library(raster)
+library(rgdal)
+
+setwd("~/Documents/GitRepos/UrbanStreamflow")
+
+files<- list.files(path = '~/Documents/GitRepos/UrbanStreamflow/BRB/', all.files=TRUE, full.name= TRUE)
+rst<-raster(file[1])
