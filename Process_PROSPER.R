20b07a496fc094b1f7f16f1376176cc157082b28
Kendra Kaiser <kendrakaiser@boisestate.edu>
2019-05-17 14:19:52 -0600

    Import and project raster

--- Process_PROSPER.R
+++ Process_PROSPER.R
@@ -6,6 +6,18 @@ library(raster)
 library(rgdal)
 
 setwd("~/Documents/GitRepos/UrbanStreamflow")
+ws <- readOGR('~/Documents/GitRepos/UrbanStreamflow/BRB/WatershedBoundary.shp')
+crs_shp<-crs(ws)
 
-files<- list.files(path = '~/Documents/GitRepos/UrbanStreamflow/BRB', all.files=TRUE, full.name= TRUE)
-rst<-raster(file[1])
+files<- list.files(path = '~/Documents/GitRepos/UrbanStreamflow/BRB', all.files=TRUE, full.name= TRUE, pattern = "\\.tif$")
+rst<-raster(files[1])
+
+rst_p<-projectRaster(rst, crs = crs_shp)
+
+
+plot(ws[4,])
+plot(rst)
+plot(ws, add = TRUE)
+
+
+lowerBoise <- crop(rst, ws[1,])
