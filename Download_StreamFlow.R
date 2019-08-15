7f99cf1fbb7f3b338b63a2f47104eed36086bd0e
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-08-15 12:56:53 -0600

    Updating some plotting for QAQC

--- Download_StreamFlow.R
+++ Download_StreamFlow.R
@@ -58,18 +58,23 @@ url_and <- "daily_precip_and.htm"# Assign URL - Anderson Ranch
 data_and <- readHTMLTable(url_and, header=TRUE, as.data.frame = TRUE, stringsAsFactors=FALSE)
 and=data_and[[1]]
 
-url_luc <- "daily_luc_spring.htm"#Lucky Peak "luc_daily.htm"
+url_luc <- "luc_daily.htm"#Lucky Peak "luc_daily.htm"
 data_luc <- readHTMLTable(url_luc, header=TRUE, as.data.frame = TRUE, stringsAsFactors=FALSE)
 luc=data_luc[[1]]
+luc<-data.frame("Date"= as.Date(luc[,1]), "lucQ"=as.numeric(luc$luc_qd), "lucIn"=as.numeric(luc$luc_id), "luc_unreg"=as.numeric(luc$luc_qu))
 
-url_nyc<- "nyc_daily.htm"#new york canal
+luc_spring=luc[7394:7609,]
+url_nyc<- "NYC_tenyear.htm"#new york canal
 data_nyc<- readHTMLTable(url_nyc, header=TRUE, as.data.frame = TRUE, stringsAsFactors=FALSE)
 nyc=data_nyc[[1]]
 
-luc_spring<-data.frame("Date"= as.Date(luc[,1]), "lucQ"=as.numeric(luc$luc_qd), "lucI"=as.numeric(luc$luc_id))
-plot(luc_spring$Date, luc_spring$lucQ, type='l')
+plot(luc_spring$Date, luc_spring$lucIn, type='l', ylim=c(0,12000))
+lines(luc$Date, luc$luc_unreg, col="blue")
+
 nyc_spring<-data.frame("Date"= as.Date(nyc[,1]),"nycQ"=as.numeric(nyc$bsei_qj))
+
 plot(nyc_spring$Date, nyc_spring$nycQ, type='l')
+
 res<-data.frame("Date"= as.Date(and[,1]), "andP"=as.numeric(and$and_pp), "arkP" = as.numeric(ark$ark_pp), "in_computed_ark"=as.numeric(ark$ark_id), "lucQ" = as.numeric(luc$luc_qd), "in_computed_LUC"=as.numeric(luc$luc_id), "in_unreg_LUC"= as.numeric(luc$luc_qu))
 
 
@@ -88,3 +93,66 @@ axis.POSIXct(1, at = seq(start, end, by="weeks"), format = "%m-%d")
 
 axis(1, Managed$dateTime, format(Managed$dateTime, "%d %d"), cex.axis = .7)
 
+library(lubridate) # work with dates
+library(dplyr) 
+
+nyc_spring$doy<-yday(nyc_spring$Date)
+nyc_daily<-group_by(nyc_spring, doy)
+nycQmean<-summarize(nyc_daily, mean(nycQ, na.rm=TRUE))
+nycQsd<-summarize(nyc_daily, sd(nycQ, na.rm=TRUE))
+names(nycQmean)<- c("doy", "mean")
+names(nycQsd)<- c("doy", "sd")
+nycQmean$sdl<- nycQmean$mean - nycQsd$sd
+nycQmean$sdu<- nycQmean$mean + nycQsd$sd
+nycQmean$mo<- month(nyc_spring$Date[93:458])
+
+plot(luc$Date[93:458], nycQmean$mean, type='l', ylim= c(0, 2500), xlab="Date", ylab="Discharge (cfs)")
+lines(luc$Date[93:458], nycQmean$sdl)
+lines(luc$Date[93:458], nycQmean$sdu)
+
+luc$lucIn[which(luc$lucIn < 0)]=0
+
+plot(luc$Date, luc$lucIn, type='l', xlab="Date", ylab="Discharge (cfs)")#, ylim=c(0,16000))
+lines(luc$Date, luc$luc_unreg, col="blue")
+
+luc$doy<-yday(luc$Date)
+luc_daily<-group_by(luc, doy)
+
+lucQ<-summarize(luc_daily, mean(lucIn, na.rm=TRUE))
+lucQsd<-summarize(luc_daily, sd(lucIn, na.rm=TRUE))
+names(lucQ)<- c("doy", "mean")
+names(lucQsd)<- c("doy", "sd")
+lucQ$sdl<-lucQ$mean - lucQsd$sd
+lucQ$sdu<- lucQ$mean + lucQsd$sd
+
+plot(lucQ$doy, lucQ$mean, type='l', ylim= c(0, 10000), xlab="Date", ylab="Discharge (cfs)")
+lines(lucQ$doy,lucQ$sdl)
+lines(lucQ$doy, lucQ$sdu)
+
+lucQout<-summarize(luc_daily, mean(lucQ, na.rm=TRUE))
+lucQsdout<-summarize(luc_daily, sd(lucQ, na.rm=TRUE))
+names(lucQout)<- c("doy", "mean")
+names(lucQsdout)<- c("doy", "sd")
+lucQout$sdl<-lucQout$mean - lucQsdout$sd
+lucQout$sdu<- lucQout$mean + lucQsdout$sd
+
+lucQur<-summarize(luc_daily, mean(luc_unreg, na.rm=TRUE))
+lucQsdur<-summarize(luc_daily, sd(luc_unreg, na.rm=TRUE))
+names(lucQur)<- c("doy", "mean")
+names(lucQsdur)<- c("doy", "sd")
+lucQur$sdl<-lucQur$mean - lucQsdur$sd
+lucQur$sdu<- lucQur$mean + lucQsdur$sd
+
+
+
+plot(lucQ$doy, lucQ$mean, type='l', ylim= c(0, 10000), xlab="Date", ylab="Discharge (cfs)", col='blue')
+lines(lucQ$doy,lucQ$sdl, col='blue')
+lines(lucQ$doy, lucQ$sdu, col='blue')
+
+plot(luc$Date[93:458], lucQur$mean, type='l', ylim= c(0, 14000), xlab="Date", ylab="Discharge (cfs)", col='blue')
+lines(luc$Date[93:458],lucQur$sdl, col='blue')
+lines(luc$Date[93:458], lucQur$sdu, col='blue')
+
+lines(luc$Date[93:458], lucQout$mean)
+lines(luc$Date[93:458],lucQout$sdl)
+lines(luc$Date[93:458], lucQout$sdu)
